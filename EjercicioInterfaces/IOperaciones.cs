f61e9cff08506efb0f77a4b0c015ed2feeff4106
kathy_chell <chela.lara76@gmail.com>
2024-10-30 15:30:42 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioInterfaces/IOperaciones.cs
@@ -0,0 +1,12 @@
+﻿
+namespace EjercicioInterfaces
+{
+    public interface IOperaciones
+    {
+       public double AreaCirculo(double radio);
+       public double AreaRectangulo(double largo, double ancho);
+       public  double VolumenCubo(double lado);
+       public double LongitudCircunferencia(double radio);
+       public double AreaTriangulo(double baseTriangulo, double altura);
+    }
+}
---
+++ EjercicioInterfaces/Operaciones.cs
@@ -0,0 +1,36 @@
+﻿
+namespace EjercicioInterfaces
+{
+    public class Operaciones : IOperaciones
+    {
+        public double AreaCirculo(double radio)
+        {
+            return Math.PI * radio * radio;
+        }
+
+        public double AreaRectangulo(double largo, double ancho)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double AreaTriangulo(double baseTriangulo, double altura)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Imprimir()
+        {
+
+        }
+
+        public double LongitudCircunferencia(double radio)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double VolumenCubo(double lado)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
